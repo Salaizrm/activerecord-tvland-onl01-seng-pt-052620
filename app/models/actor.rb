cf592a42205893ac7dc6af712b80932f440b4b75
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-16 23:01:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,17 @@
+
 class Actor < ActiveRecord::Base
+
   has_many :characters
-  has_many :characters, through: :shows
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |c|
+     "#{c.name} - #{c.show.name}"
+    end.join
+  end
+
 end
