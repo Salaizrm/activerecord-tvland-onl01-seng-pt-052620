f88aae89f398f563e8839d732acb2054c19c29cc
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-16 22:53:53 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,9 @@
 class CreateActors < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :actors
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
